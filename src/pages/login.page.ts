8548a7f463a55f35073f7a5ac4e82a5c224a9c87
KatarzynaK <kasiatesterkaqa@gmail.com>
2026-02-15 13:02:48 +0100

    test: added negative login TC

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,13 +1,15 @@
 import { LoginFormComponent } from '../components/login-form.component';
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   url = '/login/';
   loginFormComponent = new LoginFormComponent(this.page);
+  errorLoginMessage: Locator;
 
   constructor(page: Page) {
     super(page);
+    this.errorLoginMessage = this.page.getByTestId('login-error');
   }
 
   async loginUser(userName: string, userPassword: string): Promise<void> {
