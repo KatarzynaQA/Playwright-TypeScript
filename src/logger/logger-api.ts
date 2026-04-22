976f70491aa2737b6bb4c8bfab4e14530e3d4617
KatarzynaK <kasiatesterkaqa@gmail.com>
2026-04-22 12:51:52 +0200

    feat: add logging functions for console, debug, error, warning, and info

---
+++ src/logger/logger-api.ts
@@ -0,0 +1,19 @@
+// export function logConsole(message: string, args?: unknown): void {
+//   console.log(`[LOG]: ${message}`, args);
+// }
+
+// export function logDebug(message: string, args?: unknown): void {
+//   console.debug(`[DEBUG]: ${message}`, args);
+// }
+
+// export function logError(message: string, args?: unknown): void {
+//   console.error(`[ERROR]: ${message}`, args);
+// }
+
+// export function logWarning(message: string, args?: unknown): void {
+//   console.warn(`[WARNING]: ${message}`, args);
+// }
+
+// export function logInfo(message: string, args?: unknown): void {
+//   console.info(`[INFO]: ${message}`, args);
+// }
