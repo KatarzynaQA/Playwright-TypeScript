8023ddd8a7e9c1c61d6aac190b832ee304eb5e76
KatarzynaK <kasiatesterkaqa@gmail.com>
2026-04-23 13:52:53 +0200

    feat: implement ArticlesRequest class for API interactions

---
+++ src/api/requests/articles.request.ts
@@ -0,0 +1,14 @@
+import { apiUrl } from '../utils/api.utils';
+import { APIRequestContext, APIResponse } from '@playwright/test';
+
+export class ArticlesRequest {
+  url: string;
+
+  constructor(protected request: APIRequestContext) {
+    this.url = apiUrl.articlesUrl;
+  }
+
+  async get(): Promise<APIResponse> {
+    return await this.request.get(this.url);
+  }
+}
